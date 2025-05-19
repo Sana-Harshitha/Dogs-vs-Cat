8b456ac0270bee91c86ee037d961483cc2bc55db
Sana-Harshitha <sanaharshitha2@gmail.com>
2025-05-19 17:04:20 +0530

    again

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
+import os
 import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from PIL import Image
 import numpy as np
 
+MODEL_PATH = "cat_dog_model.h5"
+MODEL_FILE_ID = "1vjQkv8Wplq1JtQyFHEBH-G3tg7mZFxT2"  
+
+# Download model from Google Drive if not present
+if not os.path.exists(MODEL_PATH):
+    st.info("Downloading model. Please wait...")
+    import gdown
+    gdown.download(f"https://drive.google.com/uc?id={MODEL_FILE_ID}", MODEL_PATH, quiet=False)
+
+# Load the model
+model = load_model(MODEL_PATH)
+
+
 # Load your trained model
-model = load_model('cat_dog_model.h5')
+# model = load_model('cat_dog_model.h5')
 
 st.title("Cats vs Dogs Classifier")
 uploaded_file = st.file_uploader("Upload an image of a cat or dog...", type=["jpg", "jpeg", "png"])
